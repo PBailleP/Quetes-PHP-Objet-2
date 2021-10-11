b6e4f70a3cdc865f06c6e7c7fb0b463d3fa907ec
Pierre Bailly <82147501+PBailleP@users.noreply.github.com>
2021-10-11 11:32:31 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Bicycle.php';
+$bike = new Bicycle("Black", 1);
+echo $bike->forward();
+var_dump($bike);
+
+
+require_once 'SimpsonsCars.php';
+$car = new SimpsonsCars("blue", 4, "electric");
+var_dump($car);
+
+require_once 'Camion.php';
+$cam1 = new Camion("Red", 2, "electric", 500);
+var_dump($cam1);
+echo $cam1->filling(500);
